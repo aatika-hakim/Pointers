089da5be27c1c6b14a076f74013943d52be98330
Aatikahakim <Aatikahakim@gmail.com>
2023-03-23 13:48:26 +0500

    finding maximum subarray  sum

---
+++ max_subarray_sum.js
@@ -0,0 +1,19 @@
+// Given an array and a number k, find the sum of the subarray that has the maximum sum among all the subarrays of size k.
+
+const maxSubSum = (arr) => {
+    let maxSum = 0;
+    let prefixSum = 0;
+
+    for (let element of arr) { // for each element of arr
+         // add it to prefixSum
+        prefixSum += element;
+        maxSum = Math.max(maxSum, prefixSum); 
+        // zero if negative
+    if (prefixSum < 0) 
+        {
+        prefixSum = 0;
+        } 
+    }
+    return maxSum;
+}
+console.log(maxSubSum([2,3,6,9,3,-1,5,8]));
